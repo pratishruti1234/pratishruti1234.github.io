f9266ed9941eadd87bec2e3e7498ba487308f6e0
pratishruti1234 <pratishrutipanda0102@gmail.com>
2020-09-02 20:54:21 +0530

    Updates

---
+++ precache-manifest.0363a32843c5b569f7f0215101494fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3e9b477b3f08e132634a390f92118e",
+    "url": "/props-connetion/index.html"
+  },
+  {
+    "revision": "f8ca851fbb22cc2241a8",
+    "url": "/props-connetion/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5cc2a86b34784f98945",
+    "url": "/props-connetion/static/js/2.28ef8acb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/props-connetion/static/js/2.28ef8acb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ca851fbb22cc2241a8",
+    "url": "/props-connetion/static/js/main.b5cecdbe.chunk.js"
+  },
+  {
+    "revision": "6fc442bbdece7b3836bb",
+    "url": "/props-connetion/static/js/runtime-main.bff55749.js"
+  }
+]);
